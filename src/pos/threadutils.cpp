f14e13fb877a47c3199142e0d0d7992b750fc92f
Giacomo Milligan <giaki3003@gmail.com>
2021-04-07 20:03:19 +0200

    [misc] Avoid including linux headers on a windows environment

--- src/pos/threadutils.cpp
+++ src/pos/threadutils.cpp
@@ -7,7 +7,47 @@
 #include <pos/threadutils.h>
 
 // for priority on linux
+#ifndef WIN32
+// for posix_fallocate
+#ifdef __linux__
+
+#ifdef _POSIX_C_SOURCE
+#undef _POSIX_C_SOURCE
+#endif
+
+#define _POSIX_C_SOURCE 200112L
+
+#endif // __linux__
+
 #include <sys/resource.h>
+
+#else
+
+#ifdef _MSC_VER
+#pragma warning(disable : 4786)
+#pragma warning(disable : 4804)
+#pragma warning(disable : 4805)
+#pragma warning(disable : 4717)
+#endif
+
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+#define _WIN32_WINNT 0x0501
+
+#ifdef _WIN32_IE
+#undef _WIN32_IE
+#endif
+#define _WIN32_IE 0x0501
+
+#define WIN32_LEAN_AND_MEAN 1
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <io.h> /* for _commit */
+#include <shlobj.h>
+#endif
 
 void SetThreadPriority(int nPriority)
 {
