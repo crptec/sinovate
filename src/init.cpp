c869db3ef80614060fa6120ab633c8d02dc6b158
Giacomo Milligan <giaki3003@gmail.com>
2021-04-27 14:15:35 +0200

    [sin] Disable staking by default

--- src/init.cpp
+++ src/init.cpp
@@ -104,7 +104,7 @@ static bool fFeeEstimatesInitialized = false;
 static const bool DEFAULT_PROXYRANDOMIZE = true;
 static const bool DEFAULT_REST_ENABLE = false;
 static const bool DEFAULT_STOPAFTERBLOCKIMPORT = false;
-static const bool DEFAULT_STAKING = true;
+static const bool DEFAULT_STAKING = false;
 
 #ifdef WIN32
 // Win32 LevelDB doesn't use filedescriptors, and the ones used for
