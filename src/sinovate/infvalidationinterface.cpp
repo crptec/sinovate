638b1a55be319ed69b046dad1665d949b7893ae5
crptec <crptec.fr@gmail.com>
2021-05-05 22:48:32 +0200

    [sin] add DIN Validaion Interface
    Signed-off-by: crptec <crptec.fr@gmail.com>

---
+++ src/sinovate/infvalidationinterface.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2018-2019 SIN developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <sinovate/infvalidationinterface.h>
+#include <sinovate/infinitynodetip.h>
+#include <sinovate/infinitynodeman.h>
+#include <sinovate/infinitynodepeer.h>
+#include <sinovate/infinitynodelockreward.h>
+
+InfValidationInterface::InfValidationInterface(CConnman& connmanIn): connman(connmanIn)
+{
+    fRegister = true;
+}
+
+InfValidationInterface::~InfValidationInterface()
+{
+    fRegister = false;
+}
+
+void InfValidationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload)
+{
+    if (pindexNew == pindexFork) // blocks were disconnected without any new ones
+        return;
+
+    if (fInitialDownload)
+        return;
+
+    /*notification for infnodeman when sync is finised*/
+    infTip.UpdatedBlockTip(pindexNew, fInitialDownload, connman);
+    /*notification update block*/
+    infinitynodePeer.UpdatedBlockTip(pindexNew);
+    inflockreward.UpdatedBlockTip(pindexNew, connman);
+    /*nCachedBlockHeight of infnodeman is updated in updateFinalList which is called on validation*/
+    //infnodeman.UpdatedBlockTip(pindexNew);
+}
+
+InfValidationInterface* g_inf_validation_interface = NULL;
+
