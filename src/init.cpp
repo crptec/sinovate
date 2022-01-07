67da256e17e3e66025effe9cfcf49d46e2d4b206
hardwarewise <cryptoatif@gmail.com>
2022-01-07 18:58:12 +0300

    Fix Copyright year in License Info

--- src/init.cpp
+++ src/init.cpp
@@ -627,7 +627,7 @@ std::string LicenseInfo()
 {
     const std::string URL_SOURCE_CODE = "<https://github.com/SINOVATEblockchain/sinovate>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2018, COPYRIGHT_YEAR) + " ") + "\n" +
+     return CopyrightHolders(strprintf(_("Copyright (C) %i").translated, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software.").translated,
